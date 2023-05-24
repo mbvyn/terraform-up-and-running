ea6f4c8af27eaac95a07cf55ec7b9bf509fdb53d
MBVyn <qbodia@gmail.com>
2023-05-24 18:26:09 +0300

    Create ASG

--- main.tf
+++ main.tf
@@ -8,15 +8,21 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  description = "The public IP address of the web server"
-  value       = aws_instance.example.public_ip
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-0fb653ca2d3203ac1"
+  instance_type      = "t2.micro"
+  security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -24,10 +30,23 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  user_data_replace_on_change = true
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  max_size = 2
+  min_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
