5ff1c86b5440f67c5b265a4cb19e97b10c37cafa
MBVyn <qbodia@gmail.com>
2023-05-28 19:44:50 +0300

    Add workspace example

---
+++ workspaces/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    key = "workspaces/terraform.tfstate"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+}
