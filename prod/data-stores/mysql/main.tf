3b455811ada2c78400ce15b4dbf39390f3c670e5
MBVyn <qbodia@gmail.com>
2023-06-02 20:29:26 +0300

    Add modules and separate envs

---
+++ prod/data-stores/mysql/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mbvyn-terraform-state-prod"
+    key    = "prod/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "mbvyn-terraform-locks"
+    encrypt        = true
+
+  }
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -18,9 +18,9 @@ module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
   cluster_name = "webserver-stage"
-  db_remote_state_bucket = "mbvyn-terraform-state"
-  db_remote_state_key = "stage/data-store/mysql/terraform.tfstate"
+  db_remote_state_bucket = "mbvyn-terraform-state-stage"
+  db_remote_state_key = "prod/data-store/mysql/terraform.tfstate"
   instance_type = "t2.micro"
   max_size = 2
-  min_size = 2
+  min_size = 10
 }
