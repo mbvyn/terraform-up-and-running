faf35ac310ba3d986ca2274728606b45d82c04fc
MBVyn <qbodia@gmail.com>
2023-06-02 20:57:50 +0300

    Add separation to the envs

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -17,10 +17,28 @@ module "webserver_cluster" {
 
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webserver-stage"
+  cluster_name           = "webserver-stage"
   db_remote_state_bucket = "mbvyn-terraform-state-stage"
-  db_remote_state_key = "prod/data-store/mysql/terraform.tfstate"
-  instance_type = "t2.micro"
-  max_size = 2
-  min_size = 10
+  db_remote_state_key    = "prod/data-store/mysql/terraform.tfstate"
+  instance_type          = "t2.micro"
+  max_size               = 2
+  min_size               = 10
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
 }
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -17,10 +17,20 @@ module "webserver_cluster" {
 
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webserver-stage"
+  cluster_name           = "webserver-stage"
   db_remote_state_bucket = "mbvyn-terraform-state"
-  db_remote_state_key = "stage/data-store/mysql/terraform.tfstate"
-  instance_type = "t2.micro"
-  max_size = 2
-  min_size = 2
+  db_remote_state_key    = "stage/data-store/mysql/terraform.tfstate"
+  instance_type          = "t2.micro"
+  max_size               = 2
+  min_size               = 2
+}
+
+resource "aws_security_group" "allow_testing_inbounds" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0./0"]
 }
