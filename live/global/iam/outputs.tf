dcff1df5935a2ac516b233b57f4aca6651fc5048
MBVyn <qbodia@gmail.com>
2023-06-06 20:16:23 +0300

    Work with for_each

--- live/global/iam/outputs.tf
+++ live/global/iam/outputs.tf
@@ -1,9 +1,7 @@
-output "first_arn" {
-  value       = aws_iam_user.example[0].arn
-  description = "The ARN for the first user"
+output "all_arns" {
+  value = values(aws_iam_user.example)[*].arn
 }
 
-output "all_arns" {
-  value       = aws_iam_user.example[*].arn
-  description = "The ARNs for all users"
+output "all_users" {
+  value = aws_iam_user.example
 }
