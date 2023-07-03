bfd5ffdfce96d84329fda23d7fee08b2fe2a938a
MBVyn <qbodia@gmail.com>
2023-07-03 19:35:08 +0300

    Run k8s example

---
+++ examples/kubernetes-local/main.tf
@@ -0,0 +1,16 @@
+module "simple_webapp" {
+  source = "../../modules/services/k8s-app"
+
+  name           = "simple-webapp"
+  image          = "training/webapp"
+  replicas       = 2
+  container_port = 5000
+
+  environment_variables = {
+    PROVIDER = "Terraform"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
