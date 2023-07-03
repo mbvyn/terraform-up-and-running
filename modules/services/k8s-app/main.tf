2d6fd7fd3b696ae400dee592dc775d64ab8835a7
MBVyn <qbodia@gmail.com>
2023-07-03 19:18:41 +0300

    Create k8s module

--- modules/services/k8s-app/main.tf
+++ modules/services/k8s-app/main.tf
@@ -47,5 +47,25 @@ resource "kubernetes_deployment" "app" {
         }
       }
     }
+
+    selector {
+      match_labels = local.pod_labels
+    }
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = var.name
+  }
+
+  spec {
+    type = "LoadBalancer"
+    port {
+      port        = 80
+      target_port = var.container_port
+      protocol    = "TCP"
+    }
+    selector = local.pod_labels
   }
 }
